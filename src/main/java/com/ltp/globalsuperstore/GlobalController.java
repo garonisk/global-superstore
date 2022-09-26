4f188fd47c9465936e613dd8db592009e1393025
zesik1 <kongaronis@gmail.com>
2022-09-26 22:15:14 +0300

    adding item pojo + handle submit form

--- src/main/java/com/ltp/globalsuperstore/GlobalController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalController.java
@@ -3,11 +3,16 @@ package com.ltp.globalsuperstore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GlobalController {
     @GetMapping("/")
     public String getForm(Model model){
+        model.addAttribute("item" , new Item());
         model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
     }
@@ -15,5 +20,14 @@ public class GlobalController {
     @GetMapping("/inventory")
     public String getInventory() {
         return "inventory";
+    }
+
+    private List<Item> items = new ArrayList<Item>();
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(Item item) {
+        items.add(item);
+        return "redirect:/inventory";
+
     }
 }
