eb7bf740da8e3b750e2566db185a121bff91d5f9
zesik1 <overdrivemono@gmail.com>
2022-09-29 12:52:58 +0300

    implementing updates within 5 days + error messages

--- src/main/java/com/ltp/globalsuperstore/GlobalController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalController.java
@@ -5,9 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class GlobalController {
@@ -29,13 +32,17 @@ public class GlobalController {
 
 
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item) {
+    public String handleSubmit(Item item , RedirectAttributes redirectAttributes) {
         int index = getIndexFromId(item.getId());
+        String status = Constants.SUCCESS_STATUS;
         if (index == Constants.NOT_FOUND) {
             items.add(item);
-        } else {
+        } else if (within5Days(item.getDate(),items.get(index).getDate())){
             items.set(index,item);
+        } else {
+            status = Constants.FAILED_STATUS;
         }
+        redirectAttributes.addFlashAttribute("status", status);
         return "redirect:/inventory";
     }
 
@@ -44,5 +51,10 @@ public class GlobalController {
             if (items.get(i).getId().equals(id)) return i;
         }
         return Constants.NOT_FOUND;
+    }
+
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
     }
 }
