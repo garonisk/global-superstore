8c246d5f2f7be8595bd310bffef33bfe01c4be8b
zesik1 <overdrivemono@gmail.com>
2022-10-02 16:36:41 +0300

    implementing field validations

--- src/main/java/com/ltp/globalsuperstore/GlobalController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalController.java
@@ -2,11 +2,13 @@ package com.ltp.globalsuperstore;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,7 +20,7 @@ public class GlobalController {
     public String getForm(Model model, @RequestParam (required = false) String id){
         int index = getIndexFromId(id);
         model.addAttribute("item" ,index == Constants.NOT_FOUND ? new Item() : items.get(index));
-        model.addAttribute("categories", Constants.CATEGORIES);
+
         return "form";
     }
 
@@ -32,7 +34,12 @@ public class GlobalController {
 
 
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item , RedirectAttributes redirectAttributes) {
+    public String handleSubmit(@Valid Item item , BindingResult result, RedirectAttributes redirectAttributes) {
+        if (item.getPrice() < item.getDiscount()){
+            result.rejectValue("price","","Price cannot be less than discount");
+        }
+        if (result.hasErrors()) return "form";
+
         int index = getIndexFromId(item.getId());
         String status = Constants.SUCCESS_STATUS;
         if (index == Constants.NOT_FOUND) {
