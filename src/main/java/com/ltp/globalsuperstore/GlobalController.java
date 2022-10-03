bed85d65583d07e66a688955557e2d8657f06db7
zesik1 <overdrivemono@gmail.com>
2022-10-03 15:22:34 +0300

    code refactor

--- src/main/java/com/ltp/globalsuperstore/GlobalController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalController.java
@@ -9,59 +9,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Controller
 public class GlobalController {
+
+    GlobalService globalService = new GlobalService();
     @GetMapping("/")
     public String getForm(Model model, @RequestParam (required = false) String id){
-        int index = getIndexFromId(id);
-        model.addAttribute("item" ,index == Constants.NOT_FOUND ? new Item() : items.get(index));
-
+        model.addAttribute("item" , globalService.getItemFromId(id));
         return "form";
     }
-
-    private List<Item> items = new ArrayList<Item>();
-
     @GetMapping("/inventory")
     public String getInventory(Model model) {
-        model.addAttribute("items",items);
+        model.addAttribute("items",globalService.getItems());
         return "inventory";
     }
-
-
     @PostMapping("/submitItem")
     public String handleSubmit(@Valid Item item , BindingResult result, RedirectAttributes redirectAttributes) {
         if (item.getPrice() < item.getDiscount()){
             result.rejectValue("price","","Price cannot be less than discount");
         }
         if (result.hasErrors()) return "form";
-
-        int index = getIndexFromId(item.getId());
-        String status = Constants.SUCCESS_STATUS;
-        if (index == Constants.NOT_FOUND) {
-            items.add(item);
-        } else if (within5Days(item.getDate(),items.get(index).getDate())){
-            items.set(index,item);
-        } else {
-            status = Constants.FAILED_STATUS;
-        }
+        String status = globalService.handleSubmit(item);
         redirectAttributes.addFlashAttribute("status", status);
         return "redirect:/inventory";
     }
 
-    public int getIndexFromId(String id){
-        for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getId().equals(id)) return i;
-        }
-        return Constants.NOT_FOUND;
-    }
 
-    public boolean within5Days(Date newDate, Date oldDate) {
-        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
-        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
-    }
+
+
 }
---
+++ src/main/java/com/ltp/globalsuperstore/GlobalService.java
@@ -0,0 +1,53 @@
+package com.ltp.globalsuperstore;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class GlobalService {
+
+    GlobalRepository globalRepository = new GlobalRepository();
+
+    public Item getItem(int index){
+        return globalRepository.getItem(index);
+    }
+    public void addItem(Item item){
+        globalRepository.addItem(item);
+    }
+
+    public void updateItem(Item item , int index ){
+        globalRepository.updateItem(item,index);
+    }
+
+    public List<Item> getItems(){
+        return globalRepository.getItems();
+    }
+
+    public Item getItemFromId(String id){
+        int index = getIndexFromId(id);
+        return index == Constants.NOT_FOUND ? new Item() : getItem(index);
+    }
+    public int getIndexFromId(String id){
+        for (int i = 0; i < getItems().size(); i++) {
+            if (getItem(i).getId().equals(id)) return i;
+        }
+        return Constants.NOT_FOUND;
+    }
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
+    }
+
+    public String handleSubmit(Item item){
+        int index = getIndexFromId(item.getId());
+        String status = Constants.SUCCESS_STATUS;
+        if (index == Constants.NOT_FOUND) {
+            addItem(item);
+        } else if (within5Days(item.getDate(),getItem(index).getDate())){
+            updateItem(item, index);
+        } else {
+            status = Constants.FAILED_STATUS;
+        }
+        return status;
+    }
+}
