a89e15c6b3d6ca726997c026929b498f3edc3a2c
zesik1 <kongaronis@gmail.com>
2022-10-07 00:24:27 +0300

    implementing additional unit tests

--- src/test/java/com/ltp/globalsuperstore/GlobalServiceTest.java
+++ src/test/java/com/ltp/globalsuperstore/GlobalServiceTest.java
@@ -12,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GlobalServiceTest {
@@ -45,5 +45,27 @@ public class GlobalServiceTest {
         String id = item.getId();
         Item result = globalService.getItemFromId(id);
         assertEquals(item,result);
+    }
+
+    @Test
+    public void addItemTest(){
+        Item item = new Item("Furniture","Couch",100d,10d,new java.util.Date(System.currentTimeMillis()));
+        when(globalRepository.getItems()).thenReturn(Arrays.asList(item));
+        when(globalRepository.getItem(0)).thenReturn(item);
+
+        Item newItem = new Item("Technology","Phone",300d,70d,new java.util.Date(System.currentTimeMillis()));
+        globalService.addItem(newItem);
+        verify(globalRepository,times(1)).addItem(newItem);
+    }
+
+    @Test
+    public void updateItemTest(){
+        Item item = new Item("Furniture","Couch",100d,10d,new java.util.Date(System.currentTimeMillis()));
+        when(globalRepository.getItems()).thenReturn(Arrays.asList(item));
+        when(globalRepository.getItem(0)).thenReturn(item);
+
+        item.setDiscount(22d);
+        globalService.handleSubmit(item);
+        verify(globalRepository, times(1)).updateItem(item, 0);
     }
 }
