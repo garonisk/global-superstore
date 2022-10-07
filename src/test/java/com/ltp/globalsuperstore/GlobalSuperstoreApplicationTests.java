20f57eaec893c38e3b50d1688337a633abf7f31d
zesik1 <overdrivemono@gmail.com>
2022-10-07 14:17:01 +0300

    integration testing

--- src/test/java/com/ltp/globalsuperstore/GlobalSuperstoreApplicationTests.java
+++ src/test/java/com/ltp/globalsuperstore/GlobalSuperstoreApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ltp.globalsuperstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GlobalSuperstoreApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
 	}
 
 }
